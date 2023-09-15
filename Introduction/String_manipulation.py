fd4e7c0f6035e927e88d2722bea0fb317cdff85c
kyojaidwin <skyojaidwin.11@gmail.com>
2023-09-15 12:00:00 +0530

    added stirng manipulation

---
+++ Introduction/String_manipulation.py
@@ -0,0 +1,6 @@
+sentence = "Python is a powerful programming language"
+print("Length of the sentence:", len(sentence))
+print("Uppercase:", sentence.upper())
+print("Lowercase:", sentence.lower())
+print("Words:", sentence.split())
+print("Strip",sentence.strip(" "))
